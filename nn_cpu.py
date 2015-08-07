6e90f931410da117bd343c2118bb33080eccf990
Bob Urberger <burberger@gmail.com>
2015-08-07 02:00:06 -0700

    Refactor to keep behavior in a comment network class, add main function, eliminate multiple global states

--- nn_cpu.py
+++ nn_cpu.py
@@ -3,84 +3,117 @@ import numpy as np
 from chainer import cuda, Function, FunctionSet, gradient_check, Variable, optimizers
 import chainer.functions as F
 
+# predefined globals
 n=8000
+net = None
 
-cuda.init()
+class CommentNetwork:
+    def __init__(self, mod, opt):
+        self.model = mod
+        self.optimizer = opt
 
-model=None
-if os.path.exists('model'):
-    print 'Loading model'
-    f=open('model')
-    model=pickle.load(f)
-    f.close()
-    print 'Model Loaded'
-else:
-    model= FunctionSet(
-        x_to_h = F.Linear(8, n*2),
-        h_to_h = F.Linear(n*2, n*2),
-        h_to_y = F.Linear(n*2, 8)
-    )
+    def forward_one_step(self, h, x):
+        h    = F.tanh(self.model.x_to_h(x) + self.model.h_to_h(h))
+        y    = self.model.h_to_y(h)
+        return h, y
 
-optimizer=optimizers.MomentumSGD()
-optimizer.setup(model.collect_parameters())
+    def forward(self, input_string, output_string, volatile=False):
+        h=Variable(np.zeros((1,n*2),dtype=np.float32), volatile=volatile)
+        for c in input_string:
+            bits=Variable(np.array([[bool(ord(c)&(2**i)) for i in range(8)]], dtype=np.float32), volatile=volatile) #8 bits, never all 0 for ascii
+            h, _ = self.forward_one_step(h, bits)
 
-def forward_one_step(h, x):
-    h    = F.tanh(model.x_to_h(x) + model.h_to_h(h))
-    y    = model.h_to_y(h)
-    return h, y
+        self.optimizer.zero_grads()
+        y=''
+        nullEnd=False
+        loss=0
 
-def forward(input_string, output_string, volatile=False):
-    h=Variable(np.zeros((1,n*2),dtype=np.float32), volatile=volatile)
-    for c in input_string:
-        bits=Variable(np.array([[bool(ord(c)&(2**i)) for i in range(8)]], dtype=np.float32), volatile=volatile) #8 bits, never all 0 for ascii
-        h, _ = forward_one_step(h, bits)
+        for c in output_string:
+            h, yc = self.forward_one_step(h, Variable(np.array([[0]*8], dtype=np.float32), volatile=volatile))
+            loss+=F.mean_squared_error(yc, Variable(np.array([[bool(ord(c)&(2**i)) for i in range(8)]], dtype=np.float32)))
+            if not any(yc.data[0]):
+                nullEnd=True
+            if not nullEnd:
+                y+=chr(sum([bool(round(bit))*(2**i_bit) for i_bit, bit in enumerate(yc.data[0])]))
+        _, yc = self.forward_one_step(h, Variable(np.array([[0]*8], dtype=np.float32),  volatile=volatile))
+        loss+=F.mean_squared_error(yc, Variable(np.array([[0]*8], dtype=np.float32)))
 
-    optimizer.zero_grads()
-    y=''
-    nullEnd=False
-    loss=0
+        loss.backward()
+        self.optimizer.update()
+        return y, nullEnd
 
-    for c in output_string:
-        h, yc = forward_one_step(h, Variable(np.array([[0]*8], dtype=np.float32), volatile=volatile))
-        loss+=F.mean_squared_error(yc, Variable(np.array([[bool(ord(c)&(2**i)) for i in range(8)]], dtype=np.float32)))
-        if not any(yc.data[0]):
-            nullEnd=True
-        if not nullEnd:
-            y+=chr(sum([bool(round(bit))*(2**i_bit) for i_bit, bit in enumerate(yc.data[0])]))
-    _, yc = forward_one_step(h, Variable(np.array([[0]*8], dtype=np.float32),  volatile=volatile))
-    loss+=F.mean_squared_error(yc, Variable(np.array([[0]*8], dtype=np.float32)))
+    def trainTree(self, tree): #DFS training
+        if 'children' in tree:
+            for child in tree['children']:
+                self.trainTree(child)
+                prompt=tree['body']
+                trainResponse=child['body']
+                if prompt!='[deleted]' and trainResponse!='[deleted]' and prompt and trainResponse:
+                    self.optimizer.zero_grads()
+                    givenResponse, nullEnd=self.forward(prompt, trainResponse)
+                    print '<--prompt--'+str(len(prompt))+'chars-->\n', prompt, '\n<--trainResponse--'+str(len(trainResponse))+'chars-->\n', trainResponse, '\n<--givenResponse--'+str(len(givenResponse))+'chars'+('' if nullEnd else ', truncated')+'-->\n', givenResponse, '\n\n'
 
-    loss.backward()
-    optimizer.update()
-    return y, nullEnd
+    # loop over lines in a file identifying if they contain a tree after parsing the json
+    def trainFile(self, openFile):
+        for treeText in openFile:
+            #throw away whitespace
+            if treeText.strip():
+                #print fileName, treeText
+                tree=json.loads(treeText.strip())
+                #it's a tree, let's train
+                if 'children' in tree:
+                    print 'training '+openFile.name+'\n'
+                    self.trainTree(tree)
 
-def trainTree(tree): #DFS training
-    if 'children' in tree:
-        for child in tree['children']:
-            trainTree(child)
-            prompt=tree['body']
-            trainResponse=child['body']
-            if prompt!='[deleted]' and trainResponse!='[deleted]' and prompt and trainResponse:
-                optimizer.zero_grads()
-                givenResponse, nullEnd=forward(prompt, trainResponse)
-                print '<--prompt--'+str(len(prompt))+'chars-->\n', prompt, '\n<--trainResponse--'+str(len(trainResponse))+'chars-->\n', trainResponse, '\n<--givenResponse--'+str(len(givenResponse))+'chars'+('' if nullEnd else ', truncated')+'-->\n', givenResponse, '\n\n'
+    def saveModel():
+        print 'Stopped computation, saving model. Please wait...'
+        f=open('model','w')
+        pickle.dump(self.model, f)
+        f.close()
+        print 'Saved model'
 
 def sig_exit(_1, _2):
-    print 'Stopped computation, saving model. Please wait...'
-    f=open('model','w')
-    pickle.dump(model, f)
-    f.close()
-    print 'Saved model'
-    exit()
-signal.signal(signal.SIGINT, sig_exit)
+    saveModel()
 
-for fileName in os.listdir('trees/'):
-    if '.' not in fileName:
-        with open('trees/'+fileName) as f:
-            for treeText in f:
-                if treeText.strip():
-                    #print fileName, treeText
-                    tree=json.loads(treeText.strip())
-                    if 'children' in tree:
-                        print 'training '+fileName+'\n'
-                        trainTree(tree)
+
+# Runs the neural net
+def main():
+
+    cuda.init()
+
+    model=None
+    if os.path.exists('model'):
+        #load preexisting model history
+        print 'Loading model'
+        f=open('model')
+        model=pickle.load(f)
+        f.close()
+        print 'Model Loaded'
+    else:
+        #construct network model
+        model= FunctionSet(
+            x_to_h = F.Linear(8, n*2),
+            h_to_h = F.Linear(n*2, n*2),
+            h_to_y = F.Linear(n*2, 8)
+        )
+
+    #network weight optimizer
+    optimizer=optimizers.MomentumSGD()
+    optimizer.setup(model.collect_parameters())
+
+    net = CommentNetwork(model, optimizer)
+
+    #register ctrl-c behavior
+    signal.signal(signal.SIGINT, sig_exit)
+
+    # go find comment trees to parse
+    for fileName in os.listdir('trees/'):
+        if '.' not in fileName:
+            with open('trees/'+fileName) as f:
+                net.trainFile(f)
+
+    print 'Made it through everything, stopping...'
+    saveAndQuit()
+
+if __name__ == "__main__":
+    main()
