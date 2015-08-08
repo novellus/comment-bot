d321d1edf080229dc07c43a85629e2b3e7cce313
Novellus <novellus.cato@gmail.com>
2015-08-07 21:43:40 -0700

    much cleanup and commenting. Added truncate size option to forward, and no longer truncate output at training output size.

--- nn_cpu.py
+++ nn_cpu.py
@@ -6,7 +6,8 @@ import chainer.functions as F
 
 class CommentNetwork:
     def __init__(self, n, opt, mod=None):
-        if self.model==None
+        self.n=n
+        if mod==None:
             #construct network model
             self.model= FunctionSet(
                 x_to_h = F.Linear(8, n),
@@ -17,37 +18,61 @@ class CommentNetwork:
             self.model=mod
 
         self.optimizer = opt
-        self.optimizer.setup(model.collect_parameters())
+        self.optimizer.setup(self.model.collect_parameters())
+
+        #constants
+        self.null_byte=Variable(np.array([[0]*8], dtype=np.float32))
 
     def forward_one_step(self, h, x):
         h    = F.tanh(self.model.x_to_h(x) + self.model.h_to_h(h))
         y    = self.model.h_to_y(h)
         return h, y
 
-    def forward(self, input_string, output_string, volatile=False):
-        h=Variable(np.zeros((1,n),dtype=np.float32), volatile=volatile)
+    def forward(self, input_string, output_string, truncateSize=500, volatile=False):
+        #feed variable in, ignoring output until model has whole input string
+        h=Variable(np.zeros((1,self.n),dtype=np.float32), volatile=volatile)
         for c in input_string:
             bits=Variable(np.array([[bool(ord(c)&(2**i)) for i in range(8)]], dtype=np.float32), volatile=volatile) #8 bits, never all 0 for ascii
             h, _ = self.forward_one_step(h, bits)
 
+        #prep for training
         self.optimizer.zero_grads()
-        y=''
+        y='' #output string
         nullEnd=False
         loss=0
 
+        #Read output by prompting with null bytes.; train with training output
         for c in output_string:
-            h, yc = self.forward_one_step(h, Variable(np.array([[0]*8], dtype=np.float32), volatile=volatile))
+            h, yc = self.forward_one_step(h, self.null_byte)
             loss+=F.mean_squared_error(yc, Variable(np.array([[bool(ord(c)&(2**i)) for i in range(8)]], dtype=np.float32)))
+            if not any(yc.data[0]): #null byte signifies end of sequence
+                nullEnd=True
+            if not nullEnd:
+                y+=chr(sum([bool(round(bit))*(2**i_bit) for i_bit, bit in enumerate(yc.data[0])]))
+                truncateSize-=1
+
+        #reinforce null byte as end of sequence
+        h, yc = self.forward_one_step(h, self.null_byte) 
+        loss+=F.mean_squared_error(yc, self.null_byte)
+        if not any(yc.data[0]):
+            nullEnd=True
+        if not nullEnd:
+            y+=chr(sum([bool(round(bit))*(2**i_bit) for i_bit, bit in enumerate(yc.data[0])]))
+            truncateSize-=1
+
+        #continue reading out as long as network does not terminate and we have not hit TruncateSize
+        while not nullEnd and truncateSize>0:
+            h, yc = self.forward_one_step(h, self.null_byte)
             if not any(yc.data[0]):
                 nullEnd=True
             if not nullEnd:
                 y+=chr(sum([bool(round(bit))*(2**i_bit) for i_bit, bit in enumerate(yc.data[0])]))
-        _, yc = self.forward_one_step(h, Variable(np.array([[0]*8], dtype=np.float32),  volatile=volatile))
-        loss+=F.mean_squared_error(yc, Variable(np.array([[0]*8], dtype=np.float32)))
+                truncateSize-=1
 
+        #Train
         loss.backward()
         self.optimizer.update()
-        return y, nullEnd
+        return y, nullEnd #nullEnd true if netowrk terminated output sequence. False if output sequence truncated.
 
     def trainTree(self, tree): #DFS training
         if 'children' in tree:
@@ -72,14 +97,14 @@ class CommentNetwork:
                     print 'training '+openFile.name+'\n'
                     self.trainTree(tree)
 
-    def saveModel():
+    def saveModel(self):
         print 'Stopped computation, saving model. Please wait...'
         f=open('model','w')
         pickle.dump(self.model, f)
         f.close()
         print 'Saved model'
 
-    def sig_exit(_1, _2):
+    def sig_exit(self, _1, _2):
         self.saveModel()
         exit()
 
